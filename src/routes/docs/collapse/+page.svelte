e1350173e2a16fec404d3190f2a20adca3ea4e2f
Armin <arminkheirkhahan@gmail.com>
2024-04-22 23:23:07 +0330

    add collapse

---
+++ src/routes/docs/collapse/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Collapse from '$lib/components/Collapse/Collapse.svelte';
+	let open = false;
+</script>
+
+<Collapse color="primary" bind:open>
+	<div slot="title" class=" text-xl font-medium">
+		<div class="flex justify-between">
+			<span>Focus me to see content</span>
+		</div>
+	</div>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, esse mollitia nemo et vero
+		officiis reprehenderit maxime corporis quod, necessitatibus voluptatum, consequatur qui amet
+		beatae. Fugiat laudantium possimus sint quos.
+	</p>
+</Collapse>
+<Collapse class="mt-3" disabled color="primary" bind:open plus>
+	<div slot="title" class=" text-xl font-medium">
+		<div class="flex justify-between">
+			<span>Focus me to see content</span>
+		</div>
+	</div>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, esse mollitia nemo et vero
+		officiis reprehenderit maxime corporis quod, necessitatibus voluptatum, consequatur qui amet
+		beatae. Fugiat laudantium possimus sint quos.
+	</p>
+</Collapse>
