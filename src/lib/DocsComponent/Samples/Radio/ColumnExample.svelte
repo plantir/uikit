724da84e0d89157785c4a17ef3ead73cb796e91b
dev14 <dev14@webteamwork.ca>
2024-04-29 13:06:59 +0330

    radio component document

---
+++ src/lib/DocsComponent/Samples/Radio/ColumnExample.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import ShowCode from '$lib/DocsComponent/ComponentCode/ShowCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import SimpleCode from '$lib/DocsComponent/ComponentCode/SimpleCode.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import TabItem from '$lib/components/Tabs/TabItem.svelte';
+	import { Radio, RadioGroup, Range, Switch, Table } from '$lib/index.js';
+	let activeTab = 'javascript';
+	let form: any = {};
+	let colors: any = ['primary', 'secondary', 'accent', 'success', 'warning', 'info', 'error'];
+	let value: any;
+</script>
+
+<Tabs bind:selected={activeTab}>
+	<TabItem title="Javascript" value="javascript">
+		<ShowCode language="javascript">
+			<pre>
+			<ScriptCode>
+				import Radio from '{$importDocumentSrc}/Radio.svelte';
+				import RadioGroup from '{$importDocumentSrc}/RadioGroup.svelte';
+				let value='';
+			</ScriptCode>
+			</pre>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Html" value="html">
+		<ShowCode language="svelte">
+			<TagCode name="RadioGroup" props={['bind:value','column']}>
+				<TagCode close_self name="Radio" props={['value="male"', 'label="male"']}></TagCode>
+				<TagCode close_self name="Radio" props={['value="female"', 'label="female"']}></TagCode>
+			</TagCode>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Simple" value="simple">
+		<SimpleCode>
+			<RadioGroup bind:value column>
+				<Radio value="male" label="male"></Radio>
+				<Radio value="female" label="female"></Radio>
+			</RadioGroup>
+		</SimpleCode>
+	</TabItem>
+</Tabs>
