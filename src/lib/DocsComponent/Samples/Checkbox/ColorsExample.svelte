535346eced0da271466f424fc9e65a72e44e8f14
dev14 <dev14@webteamwork.ca>
2024-04-30 09:03:26 +0330

    add checkbox Component

---
+++ src/lib/DocsComponent/Samples/Checkbox/ColorsExample.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import ShowCode from '$lib/DocsComponent/ComponentCode/ShowCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import SimpleCode from '$lib/DocsComponent/ComponentCode/SimpleCode.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import TabItem from '$lib/components/Tabs/TabItem.svelte';
+	import { Radio, RadioGroup, Range, Switch, Table } from '$lib/index.js';
+	import Checkbox from '$lib/components/Checkbox/Checkbox.svelte';
+	let activeTab = 'javascript';
+	let form: any = {};
+	let colors: any = ['primary', 'secondary', 'accent', 'success', 'warning', 'info', 'error'];
+</script>
+
+<Tabs bind:selected={activeTab}>
+	<TabItem title="Javascript" value="javascript">
+		<ShowCode language="javascript">
+			<pre>
+			<ScriptCode>
+				import Checkbox from '{$importDocumentSrc}/Checkbox.svelte';
+			</ScriptCode>
+			</pre>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Html" value="html">
+		<ShowCode language="svelte">
+			<TagCode name="div" props={['class="grid grid-cols-4 gap-4"']}>
+				<TagCode name="Checkbox" props={['label="default"']} close_self></TagCode>
+				{#each colors as color}
+					<TagCode name="Checkbox" props={[`label="${color}"`, `color="${color}"`]} close_self
+					></TagCode>
+				{/each}
+			</TagCode>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Simple" value="simple">
+		<SimpleCode>
+			<div class="grid grid-cols-4 gap-4">
+				<Checkbox label="default" />
+				{#each colors as color}
+					<Checkbox label="default" {color} />
+				{/each}
+			</div>
+		</SimpleCode>
+	</TabItem>
+</Tabs>
---
+++ src/lib/DocsComponent/Samples/Checkbox/IndeterminateExample.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import ShowCode from '$lib/DocsComponent/ComponentCode/ShowCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import SimpleCode from '$lib/DocsComponent/ComponentCode/SimpleCode.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import TabItem from '$lib/components/Tabs/TabItem.svelte';
+	import { Checkbox, Radio, RadioGroup, Range, Switch, Table } from '$lib/index.js';
+	let activeTab = 'javascript';
+	let form: any = {};
+	let colors: any = ['primary', 'secondary', 'accent', 'success', 'warning', 'info', 'error'];
+	let value: any;
+	let checked = true;
+</script>
+
+<Tabs bind:selected={activeTab}>
+	<TabItem title="Javascript" value="javascript">
+		<ShowCode language="javascript">
+			<pre>
+			<ScriptCode>
+				import Checkbox from '{$importDocumentSrc}/Checkbox.svelte';
+			</ScriptCode>
+			</pre>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Html" value="html">
+		<ShowCode language="svelte">
+			<TagCode name="Checkbox" props={['label="indeterminate"', 'indeterminate']} close_self
+			></TagCode>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Simple" value="simple">
+		<SimpleCode>
+			<Checkbox label="indeterminate" indeterminate />
+		</SimpleCode>
+	</TabItem>
+</Tabs>
