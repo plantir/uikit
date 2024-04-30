13a13a2862cb3ddccb6c5d013bf2fd8d2774bdaa
dev14 <dev14@webteamwork.ca>
2024-04-30 08:27:25 +0330

    add Dialog Component

---
+++ src/lib/DocsComponent/Samples/Dialog/DefaultExample.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import ShowCode from '$lib/DocsComponent/ComponentCode/ShowCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import SimpleCode from '$lib/DocsComponent/ComponentCode/SimpleCode.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import TabItem from '$lib/components/Tabs/TabItem.svelte';
+	import { Dialog, Drawer, Range, Switch, Table } from '$lib/index.js';
+	let activeTab = 'javascript';
+	let form: any = {};
+	let value = '';
+	let open = false;
+	function openDialog() {
+		open = true;
+	}
+</script>
+
+<Tabs bind:selected={activeTab}>
+	<TabItem title="Javascript" value="javascript">
+		<ShowCode language="javascript">
+			<pre>
+			<ScriptCode>
+				import Dialog from '{$importDocumentSrc}/Dialog.svelte';
+				let open = false;
+				function openDialog() {'{'}
+					open = true;
+				{'}'};
+			</ScriptCode>
+			</pre>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Html" value="html">
+		<ShowCode language="svelte">
+			<TagCode name="Button" one_line props={['on:click={openDialog}']}>Open Dialog</TagCode>
+			<TagCode name="Dialog" props={['bind:open']}>
+				<TagCode name="div">content of inside Dialog</TagCode>
+			</TagCode>
+		</ShowCode>
+	</TabItem>
+	<TabItem title="Simple" value="simple">
+		<SimpleCode>
+			<Button on:click={openDialog}>Open Dialog</Button>
+			<Dialog bind:open>
+				<div>
+					<div>content of inside Dialog</div>
+				</div>
+			</Dialog>
+		</SimpleCode>
+	</TabItem>
+</Tabs>
---
+++ src/routes/docs/component/dialog/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Title from '$lib/DocsComponent/Doc/Title.svelte';
+	import ListOfProps from '$lib/DocsComponent/Samples/Dialog/ListOfProps.svelte';
+	import SetupExample from '$lib/DocsComponent/Samples/Dialog/SetupExample.svelte';
+	import DefaultExample from '$lib/DocsComponent/Samples/Dialog/DefaultExample.svelte';
+</script>
+
+<div class="component-wrapper">
+	<h1>Dialog</h1>
+	<p>
+		The dialog component can be used as an interactive dialog on top of the main content area of the
+		website to show notifications and gather information using form elements from your website
+		users.
+	</p>
+	<div class="line"></div>
+	<p>
+		dialog visibility (open/close) is controlled by the open property. You can bind it to a variable
+		that other element (usually button) will toggle. You can also use the open=true attribute to
+		show open the dialog by default. Opening and closing the dialog will trigger the show and close
+		events.
+	</p>
+	<Title id="setup" title="Setup" />
+	<p>Import Dialog component in the script tag and use it.</p>
+	<SetupExample />
+	<Title id="default-Dialog" title="Default" />
+	<DefaultExample />
+	<Title id="props" title="Props" />
+	<p>this is the list of the props that Dialog can have.</p>
+	<ListOfProps />
+	<div class="py-10"></div>
+</div>
