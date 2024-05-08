657509d56f2bd2f68cae2e4da4cdc7d9020e620a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-08 23:06:14 +0430

    feat: Add Card component

---
+++ src/lib/components/Card/Card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Loading from '../Loading/Loading.svelte';
+	import El from '$lib/utils/El.svelte';
+	import type { Card } from './Card.type.ts';
+	import './Card.scss';
+	type $$Props = Card;
+	let componentName = 'card';
+    
+	$: componentClass = {
+        
+	};
+</script>
+
+<El
+	{componentName}
+	{componentClass}
+	{...$$restProps}>
+    <slot />
+</El>
---
+++ src/lib/components/Card/CardTitle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import El from '$lib/utils/El.svelte';
+	import type { CardTitle } from './CardTitle.type.js';
+	import './CardTitle.scss';
+	type $$Props = CardTitle;
+	let componentName = 'card-title';
+    
+	$: componentClass = {
+        
+	};
+</script>
+
+<El
+	tag="h2"
+	{componentName}
+	{componentClass}
+	{...$$restProps}>
+    <slot />
+</El>
