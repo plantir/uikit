3621c77529b7dea0696eb2ec5a24173c70b1b889
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-09 18:59:05 +0430

    feat: Add Page and PageHeader components

---
+++ src/lib/components/Page/PageHeader.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import type { PageHeader } from './PageHeader.type.js'
+	import { ClassMerge } from '$lib/utils/ClassMerge.js';
+	import El from '$lib/utils/El.svelte';
+	import Icon from '../Icon/Icon.svelte';
+    import './PageHeader.scss'
+    
+    type $$Props = PageHeader
+
+    export let back: boolean | undefined = false;
+    export let title: string | undefined = undefined;
+    export let subtitle: string | undefined = undefined;
+    
+    let componentName = 'page-header'
+    
+    function onBack() {
+        history.back()
+    }
+    
+    $: componentClass = {}
+
+	$: actionsClass = ClassMerge({
+        name: `${componentName}-actions`,
+	});
+
+	$: backClass = ClassMerge({
+        name: `${componentName}-back`,
+	});
+
+	$: subtitleClass = ClassMerge({
+        name: `${componentName}-subtitle`,
+	});
+
+    $: innerClass = ClassMerge({
+        name: `${componentName}-inner`
+    })
+
+    $: titleWrapperClass = ClassMerge({
+        name: `${componentName}-title-wrapper`,
+    })
+</script>
+
+<El {componentClass} {componentName} {...$$restProps}>
+    <div class={innerClass}>
+		<div class={titleWrapperClass}>
+            {#if back}
+                <button type="button" on:click={onBack} class={backClass}>
+                    <Icon name="ArrowLeftMinor" />
+                </button>
+            {/if}
+            {#if title}
+                <h1>{title}</h1>
+            {/if}
+		</div>
+        {#if $$slots.default}
+            <div class={actionsClass}>
+                <slot />
+            </div>
+        {/if}
+	</div>
+    {#if subtitle}
+        <p class={subtitleClass}>{subtitle}</p>
+    {/if}
+</El>
+
