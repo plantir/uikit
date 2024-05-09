39a6fb620a46957b1f6e6eb3c6f4e2366959c945
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-09 16:01:34 +0430

    fix: remove bind:node for now

--- src/lib/components/Card/Card.svelte
+++ src/lib/components/Card/Card.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
-	import Loading from '../Loading/Loading.svelte';
 	import El from '$lib/utils/El.svelte';
 	import type { Card } from './Card.type.ts';
 	import './Card.scss';
-	export let node: HTMLElement | undefined = undefined
+
 	type $$Props = Card;
 	let componentName = 'card';
-    
-	$: componentClass = {
-        
-	};
+
+	$: componentClass = {};
 </script>
 
-<El
-	bind:node
-	{componentName}
-	{componentClass}
-	{...$$restProps}>
-    <slot />
+<El {componentName} {componentClass} {...$$restProps}>
+	<slot />
 </El>
