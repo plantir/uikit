3026dab2252dc88427fe840ad3cbfd8d58f76b5f
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2024-05-09 21:02:42 +0430

    feat: add slots to textfiled

--- src/lib/components/TextField/TextField.svelte
+++ src/lib/components/TextField/TextField.svelte
@@ -7,6 +7,7 @@
 	} from './TextField.type.js';
 	import './TextField.scss';
 	import { ClassMerge } from '$lib/utils/ClassMerge.js';
+	import El from '$lib/utils/El.svelte';
 	type $$Props = TextField;
 	let componentName = 'text-field';
 	export let label: string | undefined = undefined;
@@ -34,6 +35,10 @@
 		warning: color == 'warning',
 		natural: color == 'natural'
 	};
+
+	$: beforeWrapper = ClassMerge({ name: `${componentName}-before-wrapper` });
+	$: afterWrapper = ClassMerge({ name: `${componentName}-after-wrapper` });
+	$: inputWrapper = ClassMerge({ name: `${componentName}-input-wrapper` });
 	$: wrapperClass = ClassMerge({ name: `${componentName}-wrapper`, staticClassess: $$props.class });
 	$: elClass = ClassMerge({ name: componentName, componentClass, staticClassess: inputClass });
 	$: labelClass = ClassMerge({ name: `${componentName}-label` });
@@ -47,12 +52,20 @@
 			</span>
 		{/if}
 	</slot>
-	<input
+	<El class={inputWrapper} >
+		<El class={beforeWrapper}>
+			<slot name = 'before' />
+		</El>
+		<input
 		{...$$restProps}
 		{inputmode}
 		{disabled}
 		bind:value
 		{placeholder}
 		class={elClass}
-	/>
+		/>
+		<El class={afterWrapper}>
+			<slot name = 'after' />
+		</El>
+	</El>
 </label>
--- src/routes/docs/textfield/+page.svelte
+++ src/routes/docs/textfield/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import TextField from '$lib/components/TextField/TextField.svelte';
 	import TextArea from '$lib/components/TextArea/TextArea.svelte';
+	import Icon from '$lib/components/Icon/Icon.svelte'
 	let value = '';
 </script>
 
@@ -10,6 +11,22 @@
 		<TextField label="label" placeholder="placeholder" bind:value></TextField>
 		<TextField label="label" placeholder="placeholder" bind:value></TextField>
 		<TextField label="label" placeholder="placeholder" bind:value></TextField>
+	</div>
+
+	<div>
+		<TextField inputClass='px-6' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='before' name ="SearchMinor" size='md' />
+			<Icon slot='after' name ="CancelSmallMinor" size='md' />
+		</TextField>
+		<TextField inputClass='px-6' size='sm' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='before' name ="ActivitiesMajor" size='md' />
+			<Icon slot='after' name ="ActivitiesMajor" size='sm' />
+		</TextField>
+		<TextField inputClass='px-8' label="label" size='lg' placeholder="placeholder" bind:value>
+
+			<Icon slot='before' name ="ActivitiesMajor" size='lg' />
+			<Icon slot='after' name ="ActivitiesMajor" size='lg' />
+		</TextField>
 	</div>
 	<TextArea
 		class="w-full"
