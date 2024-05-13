93af59545f8b2d1a514215c32c2fe547f2c6dad5
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2024-05-13 18:33:13 +0430

    🪲fix: fix textfield display bug

--- src/routes/docs/textfield/+page.svelte
+++ src/routes/docs/textfield/+page.svelte
@@ -22,6 +22,62 @@
 			<Icon slot='start' name ="ActivitiesMajor" size='md' />
 			<Icon slot='end' name ="ActivitiesMajor" size='sm' />
 		</TextField>
+		<TextField  class='' label="label" size='lg' placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='lg' />
+			<Icon slot='end' name ="ActivitiesMajor" size='lg' />
+		</TextField>
+	</div>
+	<div class="flex">
+		<TextField  class='w-full' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="SearchMinor" size='md' />
+			<Icon slot='end' name ="CancelSmallMinor" size='md' />
+		</TextField>
+		<TextField size='sm' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='md' />
+			<Icon slot='end' name ="ActivitiesMajor" size='sm' />
+		</TextField>
+		<TextField class='w-full'  label="label" size='lg' placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='lg' />
+			<Icon slot='end' name ="ActivitiesMajor" size='lg' />
+		</TextField>
+	</div>
+	<div class="grid">
+		<TextField class="w-fll" label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="SearchMinor" size='md' />
+			<Icon slot='end' name ="CancelSmallMinor" size='md' />
+		</TextField>
+		<TextField size='sm' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='md' />
+			<Icon slot='end' name ="ActivitiesMajor" size='sm' />
+		</TextField>
+		<TextField  label="label" size='lg' placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='lg' />
+			<Icon slot='end' name ="ActivitiesMajor" size='lg' />
+		</TextField>
+	</div>
+	<div class="grid grid-cols-3">
+		<TextField class="w-fll" label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="SearchMinor" size='md' />
+			<Icon slot='end' name ="CancelSmallMinor" size='md' />
+		</TextField>
+		<TextField size='sm' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='md' />
+			<Icon slot='end' name ="ActivitiesMajor" size='sm' />
+		</TextField>
+		<TextField  label="label" size='lg' placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='lg' />
+			<Icon slot='end' name ="ActivitiesMajor" size='lg' />
+		</TextField>
+	</div>
+	<div class="flex flex-col">
+		<TextField class="w-fll" label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="SearchMinor" size='md' />
+			<Icon slot='end' name ="CancelSmallMinor" size='md' />
+		</TextField>
+		<TextField size='sm' label="label" placeholder="placeholder" bind:value>
+			<Icon slot='start' name ="ActivitiesMajor" size='md' />
+			<Icon slot='end' name ="ActivitiesMajor" size='sm' />
+		</TextField>
 		<TextField  label="label" size='lg' placeholder="placeholder" bind:value>
 			<Icon slot='start' name ="ActivitiesMajor" size='lg' />
 			<Icon slot='end' name ="ActivitiesMajor" size='lg' />
