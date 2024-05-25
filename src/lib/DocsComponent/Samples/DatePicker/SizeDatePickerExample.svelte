ae89207d6cada721c3d320dfff9404ff10677126
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2024-05-25 14:27:15 +0430

    add datepicker docs

---
+++ src/lib/DocsComponent/Samples/DatePicker/SizeDatePickerExample.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import DatePicker from '$lib/components/DatePicker/DatePicker.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import SampleWrapper from '../SampleWrapper.svelte';
+</script>
+
+<SampleWrapper>
+	<div slot="javascript">
+		<pre>
+            <ScriptCode>
+                import TextField from '{$importDocumentSrc}/TextField.svelte';
+            </ScriptCode>
+        </pre>
+	</div>
+	<div slot="html">
+		<TagCode name="DatePicker" props={['size="lg"', 'label="lg"']}></TagCode>
+		<TagCode name="DatePicker" props={['size="md"', 'label="md"']}></TagCode>
+		<TagCode name="DatePicker" props={['size="sm"', 'label="sm"']}></TagCode>
+		<TagCode name="DatePicker" props={['size="xs"', 'label="xs"']}></TagCode>
+	</div>
+	<div slot="preview">
+		<div class="flex flex-wrap gap-4">
+			<DatePicker size="lg" label="lg"></DatePicker>
+			<DatePicker size="md" label="md"></DatePicker>
+			<DatePicker size="sm" label="sm"></DatePicker>
+			<DatePicker size="xs" label="xs"></DatePicker>
+		</div>
+	</div>
+</SampleWrapper>
--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -88,10 +88,10 @@ export const components = writable([
         title: 'ColorPicker',
         value: 'component/colorpicker'
     },
-    // {
-    // 	title: 'datepicker',
-    // 	value: 'component/datepicker'
-    // },
+    {
+    	title: 'datepicker',
+    	value: 'component/datepicker'
+    },
     {
         title: 'Dialog',
         value: 'component/dialog'
