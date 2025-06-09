2a8514d4441b4c57c68b9c40a463c10957da5bec
Aryabod <aryabod.kh@gmail.com>
2025-06-09 14:02:52 +0330

    fix some issue

---
+++ src/lib/DocsComponent/Samples/Swap/ListOfProps.svelte
@@ -0,0 +1,35 @@
+<script>
+	import PropTable from '$lib/DocsComponent/ComponentCode/PropTable.svelte';
+
+	let items = [
+		{
+			name: 'on',
+			type: `string`,
+			default: 'undefined'
+		},
+		{
+			name: 'off',
+			type: `string`,
+			default: 'undefined'
+		},
+		{
+			name: 'value',
+			type: `boolean`,
+			default: 'false'
+		},
+		{
+			name: 'rotate',
+			type: `boolean`,
+			default: 'false'
+		},
+		{
+			name: 'flip',
+			type: `boolean`,
+			default: 'false'
+		},
+	];
+</script>
+
+<div>
+	<PropTable {items} />
+</div>
---
+++ src/lib/DocsComponent/Samples/Swap/RotateExample.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import ScriptCode from '$lib/DocsComponent/ComponentCode/ScriptCode.svelte';
+	import ShowCode from '$lib/DocsComponent/ComponentCode/ShowCode.svelte';
+	import TagCode from '$lib/DocsComponent/ComponentCode/TagCode.svelte';
+	import SimpleCode from '$lib/DocsComponent/ComponentCode/SimpleCode.svelte';
+	import { importDocumentSrc } from '$lib/store/index.js';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import TabItem from '$lib/components/Tabs/TabItem.svelte';
+	import { Switch, Table } from '$lib/index.js';
+	let activeTab = 'Preview';
+	let form: any = {};
+	import SampleWrapper from '../SampleWrapper.svelte';
+	import Swap from '$lib/components/Swap/Swap.svelte';
+</script>
+
+<SampleWrapper>
+	<div slot="javascript">
+		<pre>
+			<ScriptCode>
+				import Swap from '{$importDocumentSrc}/Swap.svelte';
+			</ScriptCode>
+			</pre>
+	</div>
+	<div slot="html">
+		<TagCode name="div">
+			<TagCode
+				name="Swap"
+				one_line
+				props={['on="ON"', 'off="OFF"', 'value={false}', 'rotate']}
+			></TagCode>
+		</TagCode>
+	</div>
+	<div slot="preview">
+		<div>
+			<Swap value={false} on="ON" off="OFF" rotate />
+		</div>
+	</div>
+</SampleWrapper>
