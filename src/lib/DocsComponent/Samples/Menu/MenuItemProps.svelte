62f4976552bb6316755a0d0e926a075bf479fb5c
Aryabod <aryabod.kh@gmail.com>
2025-07-19 14:00:53 +0330

    add menu props

---
+++ src/lib/DocsComponent/Samples/Menu/MenuItemProps.svelte
@@ -0,0 +1,20 @@
+<script>
+	import PropTable from '$lib/DocsComponent/ComponentCode/PropTable.svelte';
+
+	let items = [
+		{
+			name: 'active',
+			type: 'boolean',
+			default: 'false'
+		},
+		{
+			name: 'disabled',
+			type: 'boolean',
+			default: 'false'
+		}
+	];
+</script>
+
+<div>
+	<PropTable {items} />
+</div>
--- src/routes/docs/component/menu/+page.svelte
+++ src/routes/docs/component/menu/+page.svelte
@@ -4,18 +4,29 @@
 	import MenuActiveExample from '$lib/DocsComponent/Samples/Menu/MenuActiveExample.svelte';
 	import MenuDefaultExample from '$lib/DocsComponent/Samples/Menu/MenuDefaultExample.svelte';
 	import MenuDisableExample from '$lib/DocsComponent/Samples/Menu/MenuDisableExample.svelte';
+	import MenuItemProps from '$lib/DocsComponent/Samples/Menu/MenuItemProps.svelte';
+	import MenuProps from '$lib/DocsComponent/Samples/Menu/MenuProps.svelte';
 	import MenuSizesExample from '$lib/DocsComponent/Samples/Menu/MenuSizesExample.svelte';
 	import MenuSubmenuCollapsableExample from '$lib/DocsComponent/Samples/Menu/MenuSubmenuCollapsableExample.svelte';
 	import MenuSubmenuExample from '$lib/DocsComponent/Samples/Menu/MenuSubmenuExample.svelte';
 	import MenuTitleExample from '$lib/DocsComponent/Samples/Menu/MenuTitleExample.svelte';
 	import MenuVerticalExample from '$lib/DocsComponent/Samples/Menu/MenuVerticalExample.svelte';
+	import SubMenuProps from '$lib/DocsComponent/Samples/Menu/SubMenuProps.svelte';
 </script>
 
 <div class="pb-10">
 	<div class="component-wrapper">
 		<h1>Menu</h1>
 	</div>
-	<DockProps />
+	<MenuProps />
+	<div class="component-wrapper">
+		<h1>MenuItem</h1>
+	</div>
+	<MenuItemProps />
+	<div class="component-wrapper">
+		<h1>Submenu</h1>
+	</div>
+	<SubMenuProps />
 	<Title id="Menu" title="Menu" />
 	<MenuDefaultExample />
 	<Title id="menu-horzintal" title="Menu Horzintal" />
